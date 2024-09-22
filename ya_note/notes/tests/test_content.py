fabfb90a816c2179af9a745ce13f68b4e15be04e
Ev Zem <jekh.zem@gmail.com>
2024-09-22 19:08:23 +0300

    внёс правки ревьювера 2.1

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -11,6 +11,7 @@ from .base_test_case import BaseTestCase  # Corrected relative import order
 
 User = get_user_model()
 
+
 @pytest.fixture
 def author(django_user_model):
     return django_user_model.objects.create(username="Автор")
@@ -19,17 +20,20 @@ def author(django_user_model):
 def not_author(django_user_model):
     return django_user_model.objects.create(username="Не автор")
 
+
 @pytest.fixture
 def author_client(author):
     client = Client()
     client.force_login(author)
     return client
 
+
 @pytest.fixture
 def not_author_client(not_author):
     client = Client()
     client.force_login(not_author)
     return client
+
 
 @pytest.fixture
 def note(author):
@@ -41,6 +45,7 @@ def note(author):
     )
     return note
 
+
 @pytest.fixture
 def slug_for_args(note):
     return (note.slug,)
@@ -49,25 +54,25 @@ def slug_for_args(note):
 class TestContentPage(BaseTestCase):
 
     def test_note_in_list_for_author(self):
-        """Отдельная заметка передаётся на страницу со списком заметок.""" 
+        """Отдельная заметка передаётся на страницу со списком заметок."""
         response = self.author_client.get(self.list_url)
         notes = response.context["object_list"]
         self.assertIn(self.note, notes)
 
     def test_note_not_in_list_for_another_user(self):
-        """В список одного пользователя не попадают заметки других юзеров.""" 
+        """В список одного пользователя не попадают заметки других юзеров."""
         response = self.another_user_client.get(self.list_url)
         notes = response.context["object_list"]
         self.assertNotIn(self.note, notes)
 
     def test_add_page_have_form(self):
-        """На странице создания заметки передаются формы.""" 
+        """На странице создания заметки передаются формы."""
         response = self.author_client.get(self.add_url)
         self.assertIn("form", response.context)
         self.assertIsInstance(response.context["form"], NoteForm)
 
     def test_edit_page_have_form(self):
-        """На странице редактирования заметки передаются формы.""" 
+        """На странице редактирования заметки передаются формы."""
         response = self.author_client.get(self.edit_url)
         self.assertIn("form", response.context)
         self.assertIsInstance(response.context["form"], NoteForm)
--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,5 +1,3 @@
-
-import pytest
 from http import HTTPStatus
 from django.urls import reverse
 from django.contrib.auth import get_user_model
